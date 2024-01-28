434442065cff7f5741ff7bb648d3b52df93675bc
James Chiang <jchiang87@gmail.com>
2024-01-28 12:02:40 -0800

    revise calib opsim db generation script

--- scripts/make_calib_opsim_db.py
+++ scripts/make_calib_opsim_db.py
@@ -9,66 +9,83 @@ import pandas as pd
 opsim_db_file = "/sdf/data/rubin/user/jchiang/imSim/rubin_sim_data/opsim_cadences/baseline_v3.2_10yrs.db"
 assert os.path.isfile(opsim_db_file)
 with sqlite3.connect(opsim_db_file) as con:
-    df = pd.read_sql("select * from observations limit 10", con)
+    df0 = pd.read_sql("select * from observations limit 10", con)
 
-# Create a template dict with default entries from the first row.
-row = dict(df.iloc[0])
+# Make a template dict with default entries from the first row.
+row = dict(df0.iloc[0])
 
-# Fill a new DataFrame with the calibration frame entries.
-df_new = pd.DataFrame(columns=df.columns)
+# Create a new DataFrame with the calibration frame entries.
+df = pd.DataFrame(columns=df0.columns)
+
+mjd = 60326.    # 2024-01-17 00:00:00
+visit = 3000000
 
 # Bias frames
-bias_id_offset = 3000000
-mjd0 = 60326.    # 2024-01-17 00:00:00
-dt = 10./86400.
-for i in range(20):
-    visit = bias_id_offset + i
-    mjd = mjd0 + dt*i
+exptime = 0
+dt = (exptime + 10.)/86400.
+nbias = 20
+for i in range(nbias):
     row.update(dict(observationId=visit,
                     observationStartMJD=mjd,
-                    visitExposureTime=0.,
+                    visitExposureTime=exptime,
                     numExposures=1,
                     target='BIAS'))
-    df_new.loc[len(df_new)] = row
+    df.loc[len(df)] = row
+    visit += 1
+    mjd += dt
 
 # Dark frames
-dark_id_offset = 4000000
-mjd0 = 60327.
-dt = 10./86400.
-for i in range(20):
-    visit = dark_id_offset + i
-    mjd = mjd0 + dt*i
+exptime = 100.
+dt = (exptime + 10.)/86400.
+ndark = 20
+for i in range(ndark):
     row.update(dict(observationId=visit,
                     observationStartMJD=mjd,
                     numExposures=1,
-                    visitExposureTime=100.,
+                    visitExposureTime=exptime,
                     target='DARK'))
-    df_new.loc[len(df_new)] = row
+    df.loc[len(df)] = row
+    visit += 1
+    mjd += dt
 
-# Flat frames
-flat_id_offset = 5000000
-mjd_last = 60328.
-dt = 10./86400.
-num_pairs = 20
-exptimes = np.logspace(np.log10(0.3), np.log10(1e3), num_pairs)
-for i, exptime in enumerate(exptimes):
-    for j in (0, 1):
-        visit = flat_id_offset + i*2 + j
-        mjd = mjd_last + exptime + dt
+# Flat frames, assume 100 counts/pixel illumination
+exptime = 500.
+dt = (exptime + 10.)/86400.
+bands = ['g', 'r', 'i']
+nflat = 20
+for band in bands:
+    for i in range(nflat):
         row.update(dict(observationId=visit,
                         observationStartMJD=mjd,
                         numExposures=1,
                         visitExposureTime=exptime,
+                        filter=band,
                         target='FLAT'))
-        df_new.loc[len(df_new)] = row
-        mjd_last = mjd
+        df.loc[len(df)] = row
+        visit += 1
+        mjd += dt
+
+# PTC frames, assume 100 counts/pixel illumination
+num_pairs = 100
+exptimes = np.logspace(np.log10(0.3), np.log10(1e3), num_pairs)
+for i, exptime in enumerate(exptimes):
+    dt = (exptime + 10.)/86400.
+    for j in (0, 1):
+        row.update(dict(observationId=visit,
+                        observationStartMJD=mjd,
+                        numExposures=1,
+                        visitExposureTime=exptime,
+                        target='PTC'))
+        df.loc[len(df)] = row
+        visit += 1
+        mjd += dt
+
 
 # Write out the new DataFrame as an sqlite file.
-
 outfile = "calib_frames.db"
 if os.path.isfile(outfile):
     if click.confirm(f"Overwrite {outfile}?", default=True):
         os.remove(outfile)
 
 with sqlite3.connect(outfile) as con:
-    df_new.to_sql("observations", con, index=False)
+    df.to_sql("observations", con, index=False)
