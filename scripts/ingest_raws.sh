60270ede8b0b1b6775844e23c69c864be58f13a0
Anand Raichoor <araichoor@cca019.in2p3.fr>
2026-03-23 12:57:13 +0100

    make REPO and RAWDIR arguments

--- scripts/ingest_raws.sh
+++ scripts/ingest_raws.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 set -xe
 
-raw_dir=/sdf/data/rubin/shared/ops-rehearsals/ops-rehearsal-4/image_sims/full_focal_plan_calibs/03*
-repo=./repo
+REPO=$1
+RAWDIR=$2
+#raw_dir=/sdf/data/rubin/shared/ops-rehearsals/ops-rehearsal-4/image_sims/full_focal_plan_calibs/03*
+#repo=./repo
 
 butler --log-level DEBUG ingest-raws --transfer direct ${repo} ${raw_dir}/*R22_S11*.fits.fz
-#butler define-visits ${repo} LSSTCamSim
+#butler define-visits ${REPO} LSSTCamSim
