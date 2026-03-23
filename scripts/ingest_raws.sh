6237c6db9a0a4ae038f7e17f83cf1dd4db1e034c
Anand Raichoor <araichoor@cca019.in2p3.fr>
2026-03-23 12:57:52 +0100

    more general amp file names

--- scripts/ingest_raws.sh
+++ scripts/ingest_raws.sh
@@ -6,5 +6,6 @@ RAWDIR=$2
 #raw_dir=/sdf/data/rubin/shared/ops-rehearsals/ops-rehearsal-4/image_sims/full_focal_plan_calibs/03*
 #repo=./repo
 
-butler --log-level DEBUG ingest-raws --transfer direct ${repo} ${raw_dir}/*R22_S11*.fits.fz
+#butler --log-level DEBUG ingest-raws --transfer direct ${repo} ${raw_dir}/*R22_S11*.fits.fz
+butler --log-level DEBUG ingest-raws --transfer direct ${REPO} ${RAWDIR}/amp*.fits.fz
 #butler define-visits ${REPO} LSSTCamSim
